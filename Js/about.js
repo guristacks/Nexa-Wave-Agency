ffbc68c9362369e928d6d9d335921f3c3f89c1c9
guristacks <guristacks@gmail.com>
2026-04-06 10:51:59 +0530

    about page completed

--- Js/about.js
+++ Js/about.js
@@ -93,7 +93,6 @@ const loadingAnimation = () => {
     duration: 0.3,
     opacity: 0,
   });
-
 };
 
 const navBarAnimation = () => {
@@ -171,419 +170,162 @@ const brandsAnimation = () => {
   }
 };
 
-// const serviceAnimation = () => {
-//   gsap.from(".servicesSec .allh2, .servicesSec .allh1, .servicesSec .allp", {
-//     y: -100,
-//     opacity: 0,
-//     scale: 1.3,
-//     scrollTrigger: {
-//       trigger: ".servicesSec",
-//       scroller: "body",
-//       start: "top 90%",
-//       end: "top 30%",
-//       scrub: 2,
-//     },
-//   });
+const whyUsAnimation = () => {
+  gsap.from(".whyUsSec .allh2, .whyUsSec .allh1, .whyUsSec .allp", {
+    y: -100,
+    opacity: 0,
+    scale: 1.3,
+    scrollTrigger: {
+      trigger: ".whyUsSec",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 30%",
+      scrub: 2,
+    },
+  });
 
-//   gsap.from(".serv1", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".serv1",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+  gsap.from(".box", {
+    opacity: 0,
+    y: 50,
+    scale: 0.7,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".box",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 70%",
+      scrub: 2,
+    },
+  });
+};
 
-//   gsap.from(".serv2", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".serv2",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+const valueAnimation = () => {
+  gsap.from(".valueSec .allh2, .valueSec .allh1, .valueSec .allp", {
+    y: -100,
+    opacity: 0,
+    scale: 1.3,
+    scrollTrigger: {
+      trigger: ".valueSec",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 30%",
+      scrub: 2,
+    },
+  });
 
-//   gsap.from(".serv3", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".serv3",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+  gsap.from(".value-box", {
+    opacity: 0,
+    y: 50,
+    scale: 0.7,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".value-box",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 70%",
+      scrub: 2,
+    },
+  });
+};
 
-//   gsap.from(".serv4", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".serv4",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
-// };
+const diffrenceAnimation = () => {
+  gsap.from(".diffrenceSec .allh2, .diffrenceSec .allh1, .diffrenceSec .allp", {
+    y: -100,
+    opacity: 0,
+    scale: 1.3,
+    scrollTrigger: {
+      trigger: ".diffrenceSec",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 30%",
+      scrub: 2,
+    },
+  });
 
-// const processAnimation = () => {
-//   gsap.from(".processSec .allh2, .processSec .allh1, .processSec .allp", {
-//     y: -100,
-//     opacity: 0,
-//     scale: 1.3,
-//     scrollTrigger: {
-//       trigger: ".processSec",
-//       scroller: "body",
-//       start: "top 90%",
-//       end: "top 30%",
-//       scrub: 2,
-//     },
-//   });
+  if (window.innerWidth > 1024) {
+    gsap.from(".landing, .elite", {
+      opacity: 0,
+      scale: 0.7,
+      scrollTrigger: {
+        trigger: ".diffrenceSec",
+        scroller: "body",
+        start: "top 90%",
+        end: "top 70%",
+        scrub: 2,
+      },
+    });
+  }
 
-//   gsap.from(".step1", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".step1",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+  if (window.innerWidth < 768) {
+    gsap.from(".landing", {
+      opacity: 0,
+      scale: 0.7,
+      scrollTrigger: {
+        trigger: ".landing",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: 2,
+      },
+    });
+  }
 
-//   gsap.from(".step2", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".step2",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+  if (window.innerWidth < 768) {
+    gsap.from(".elite", {
+      opacity: 0,
+      scale: 0.7,
+      scrollTrigger: {
+        trigger: ".elite",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: 2,
+      },
+    });
+  }
+};
 
-//   gsap.from(".step3", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".step3",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+const faqsAnimation = () => {
+  gsap.from(".faqSec .allh2, .faqSec .allh1, .faqSec .allp", {
+    y: -100,
+    opacity: 0,
+    scale: 1.3,
+    scrollTrigger: {
+      trigger: ".faqSec",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 30%",
+      scrub: 2,
+    },
+  });
 
-//   gsap.from(".step4", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".step4",
-//       scroller: "body",
-//       top: "top 70%",
-//       end: "top 60%",
-//       scrub: 2,
-//     },
-//   });
+  gsap.from(".all-ques", {
+    opacity: 0,
+    scale: 0.7,
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: ".faqSec",
+      scroller: "body",
+      start: "top 50%",
+      end: "top 20%",
+      scrub: 2,
+    },
+  });
+};
 
-// };
-
-// // const casesAnimation = () => {
-// //   gsap.from(".casesSec .allh2, .casesSec .allh1, .casesSec .allp", {
-// //     y: -100,
-// //     opacity: 0,
-// //     scale: 1.3,
-// //     scrollTrigger: {
-// //       trigger: ".casesSec",
-// //       scroller: "body",
-// //       start: "top 90%",
-// //       end: "top 30%",
-// //       scrub: 2,
-// //     },
-// //   });
-// // };
-
-// const benefitsAnimation = () => {
-//   gsap.from(".benefitsSec .allh2, .benefitsSec .allh1, .benefitsSec .allp", {
-//     y: -100,
-//     opacity: 0,
-//     scale: 1.3,
-//     scrollTrigger: {
-//       trigger: ".benefitsSec",
-//       scroller: "body",
-//       start: "top 90%",
-//       end: "top 30%",
-//       scrub: 2,
-//     },
-//   });
-
-//   gsap.from(".box", {
-//     opacity: 0,
-//     y: 50,
-//     scale: 0.7,
-//     ease: "power2.out",
-//     scrollTrigger: {
-//       trigger: ".box",
-//       scroller: "body",
-//       start: "top 90%",
-//       end: "top 70%",
-//       scrub: 2,
-//     },
-//   });
-// };
-
-// const pricingAnimation = () => {
-//   gsap.from(
-//     ".pricingSec .allh2, .pricingSec .allh1, .pricingSec .allp, .pricingMethod",
-//     {
-//       y: -100,
-//       opacity: 0,
-//       scale: 1.3,
-//       scrollTrigger: {
-//         trigger: ".pricingSec",
-//         scroller: "body",
-//         start: "top 90%",
-//         end: "top 30%",
-//         scrub: 2,
-//       },
-//     },
-//   );
-
-//   if (window.innerWidth > 1024) {
-//     gsap.from(".landing, .elite, .custom", {
-//       opacity: 0,
-//       scale: 0.7,
-//       scrollTrigger: {
-//         trigger: ".pricingSec",
-//         scroller: "body",
-//         start: "top 90%",
-//         end: "top 70%",
-//         scrub: 2,
-//       },
-//     });
-//   }
-
-//   if (window.innerWidth < 768) {
-//     gsap.from(".landing", {
-//       opacity: 0,
-//       scale: 0.7,
-//       scrollTrigger: {
-//         trigger: ".landing",
-//         scroller: "body",
-//         start: "top 70%",
-//         end: "top 50%",
-//         scrub: 2,
-//       },
-//     });
-//   }
-
-//   if (window.innerWidth < 768) {
-//     gsap.from(".elite", {
-//       opacity: 0,
-//       scale: 0.7,
-//       scrollTrigger: {
-//         trigger: ".elite",
-//         scroller: "body",
-//         start: "top 70%",
-//         end: "top 50%",
-//         scrub: 2,
-//       },
-//     });
-//   }
-
-//   if (window.innerWidth < 768) {
-//     gsap.from(".custom", {
-//       opacity: 0,
-//       scale: 0.7,
-//       scrollTrigger: {
-//         trigger: ".custom",
-//         scroller: "body",
-//         start: "top 70%",
-//         end: "top 50%",
-//         scrub: 2,
-//       },
-//     });
-//   }
-
-//   const priceMethod = () => {
-//     let toggleBtn = document.querySelector(".togglebtn");
-//     let white = document.querySelector(".white");
-//     let annually = document.querySelectorAll(".anually");
-//     let monthly = document.querySelectorAll(".monthly");
-
-//     toggleBtn.addEventListener("click", () => {
-//       toggleBtn.classList.toggle("black");
-//       white.classList.toggle("left");
-
-//       annually.forEach((el) => el.classList.toggle("remove"));
-//       monthly.forEach((el) => el.classList.toggle("remove"));
-//     });
-//   };
-
-//   priceMethod();
-// };
-
-// const testimonialsAnimation = () => {
-//   gsap.from(
-//     ".testimonialSec .allh2, .testimonialSec .allh1, .testimonialSec .allp",
-//     {
-//       y: -100,
-//       opacity: 0,
-//       scale: 1.3,
-//       scrollTrigger: {
-//         trigger: ".testimonialSec",
-//         scroller: "body",
-//         start: "top 90%",
-//         end: "top 30%",
-//         scrub: 2,
-//       },
-//     },
-//   );
-
-//   gsap.from(".Reviewrow", {
-//     opacity: 0,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".testimonialSec",
-//       scroller: "body",
-//       start: "top 50%",
-//       end: "top 20%",
-//       scrub: 2,
-//     },
-//   });
-
-//   gsap.from(".Reviewrow2", {
-//     opacity: 0,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".testimonialSec",
-//       scroller: "body",
-//       start: "top 20%",
-//       end: "top -10%",
-//       scrub: 2,
-//     },
-//   });
-
-//   if (window.innerWidth > 1024) {
-//     gsap.to(".Reviewrow", {
-//       x: "-170%",
-//       duration: 20,
-//       repeat: -1,
-//       yoyo: true,
-//       ease: "none",
-//     });
-//   }
-
-//   if (window.innerWidth > 767 && window.innerWidth < 1025) {
-//     gsap.to(".Reviewrow", {
-//       x: "-250%",
-//       duration: 20,
-//       repeat: -1,
-//       yoyo: true,
-//       ease: "none",
-//     });
-//   }
-
-//   if (window.innerWidth > 1024) {
-//     gsap.to(".Reviewrow2", {
-//       x: "170%",
-//       duration: 20,
-//       repeat: -1,
-//       yoyo: true,
-//       ease: "none",
-//     });
-//   }
-
-//   if (window.innerWidth > 767 && window.innerWidth < 1025) {
-//     gsap.to(".Reviewrow2", {
-//       x: "250%",
-//       duration: 20,
-//       repeat: -1,
-//       yoyo: true,
-//       ease: "none",
-//     });
-//   }
-
-//   if (window.innerWidth < 768) {
-//     gsap.to(".Reviewrow", {
-//       x: "-340%",
-//       duration: 20,
-//       repeat: -1,
-//       yoyo: true,
-//       ease: "none",
-//     });
-//   }
-
-//   if (window.innerWidth < 768) {
-//     gsap.to(".Reviewrow2", {
-//       x: "340%",
-//       duration: 20,
-//       repeat: -1,
-//       yoyo: true,
-//       ease: "none",
-//     });
-//   }
-// };
-
-// const faqsAnimation = () => {
-//   gsap.from(".faqSec .allh2, .faqSec .allh1, .faqSec .allp", {
-//     y: -100,
-//     opacity: 0,
-//     scale: 1.3,
-//     scrollTrigger: {
-//       trigger: ".faqSec",
-//       scroller: "body",
-//       start: "top 90%",
-//       end: "top 30%",
-//       scrub: 2,
-//     },
-//   });
-
-//   gsap.from(".all-ques", {
-//     opacity: 0,
-//     scale: 0.7,
-//     stagger: 0.3,
-//     scrollTrigger: {
-//       trigger: ".faqSec",
-//       scroller: "body",
-//       start: "top 50%",
-//       end: "top 20%",
-//       scrub: 2,
-//     },
-//   });
-// };
-
-// const promoAnimation = () => {
-//   gsap.from(".promotion", {
-//     opacity: 0,
-//     scale: 0.7,
-//     scrollTrigger: {
-//       trigger: ".promoSec",
-//       scroller: "body",
-//       start: "top 90%",
-//       end: "top 50%",
-//       scrub: 2,
-//     },
-//   });
-// };
+const promoAnimation = () => {
+  gsap.from(".promotion", {
+    opacity: 0,
+    scale: 0.7,
+    scrollTrigger: {
+      trigger: ".promoSec",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 50%",
+      scrub: 2,
+    },
+  });
+};
 
 lenisAnimation();
 
@@ -593,18 +335,12 @@ navBarAnimation();
 
 brandsAnimation();
 
-// serviceAnimation();
+whyUsAnimation();
 
-// processAnimation();
+valueAnimation();
 
-// // casesAnimation();
+diffrenceAnimation();
 
-// benefitsAnimation();
+faqsAnimation();
 
-// pricingAnimation();
-
-// testimonialsAnimation();
-
-// faqsAnimation();
-
-// promoAnimation();
+promoAnimation();
